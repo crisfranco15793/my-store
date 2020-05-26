0da177eedbebbe7ea0c87c4a6fcfc6fc2cfe5130
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2020-05-25 21:27:40 -0500

    module actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ExponentialPipe } from './pipes/exponential.pipe';
-import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProductsComponent } from './components/products/products.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
-import { BannerComponent } from './shared/components/banner/banner.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
@@ -21,13 +19,11 @@ import { LayoutComponent } from './components/layout/layout.component';
     AppComponent,
     ProductComponent,
     ExponentialPipe,
-    HomeComponent,
     ContactComponent,
     ProductsComponent,
     PageNotFoundComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent,
     ProductDetailComponent,
     LayoutComponent
   ],
